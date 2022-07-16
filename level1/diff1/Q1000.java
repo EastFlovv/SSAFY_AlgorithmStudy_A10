be37c556a581eb9dee8ba837bb0591e7fb099d8f
eastflow <rlaehddus815@gmail.com>
2022-07-16 16:09:36 +0900

    init project

---
+++ level1/diff1/Q1000.java
@@ -0,0 +1,18 @@
+package level1.diff1;
+
+import java.io.*;
+import java.util.*;
+
+public class Q1000 {
+    public static void main(String[] args) throws IOException {
+//        bufferedReader 선언
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String s = bf.readLine();
+//        stringTokenizer 사용
+        StringTokenizer st = new StringTokenizer(s);
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a + b);
+    }
+}
---
+++ level1/diff1/Q1001.java
@@ -0,0 +1,17 @@
+package level1.diff1;
+
+import java.io.*;
+import java.util.*;
+
+public class Q1001 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(input);
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a - b);
+    }
+}
