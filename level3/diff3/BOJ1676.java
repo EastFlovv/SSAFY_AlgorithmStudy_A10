bc2ca2d193762b94a5039007069a2b187a0fe450
eastflow <rlaehddus815@gmail.com>
2022-08-09 10:16:22 +0900

    fix: level3 diff3 solved: 1

--- level3/diff3/BOJ1676.java
+++ level3/diff3/BOJ1676.java
@@ -4,17 +4,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BOJ1676_notFin {
+public class BOJ1676 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringBuilder sb = new StringBuilder();
 
         int num = Integer.parseInt(br.readLine());
         int cur = 5;
         int result = 0;
-//        while (cur <= num){
-//            if(cur)
-//        }
-
+        while (cur <= num){
+            int curTmp = cur;
+            while (curTmp % 5 == 0){
+                curTmp /= 5;
+                result++;
+            }
+            cur+=5;
+        }
+        System.out.println(result);
     }
 }
